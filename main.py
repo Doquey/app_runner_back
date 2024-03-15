7ce7d69fae170f1c4ccbbb0773a140418bb63fee
Luis Filipe <luis.souza@daedalus-tech.com>
2024-03-14 21:14:03 -0300

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from schema import TaskRequest
 import cv2
@@ -12,13 +13,23 @@ S3_ACCESS_KEY_ID = os.getenv('S3_ACCESS_KEY_ID')
 S3_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
 
 
+app.add_middleware(
+    CORSMiddleware,
+    # Update this with your frontend origin(s) or "*" for all origins
+    allow_origins=["*"],
+    allow_credentials=True,
+    # Specify the HTTP methods you want to allow
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],  # Specify the HTTP headers you want to allow
+)
+
+
 @app.post("/task/")
 def handle_task(task: TaskRequest):
     bucket_name = "apprunnerimages"
 
     task_type = task.task
-    img_path = task.imgPath
-    img_name = img_path.split("/")[-1]  # may change
+    img_name = task.imgPath
     login_to_aws(S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY)
     download_image_from_s3(bucket_name, img_name, img_name)
     img = cv2.imread(img_name)
